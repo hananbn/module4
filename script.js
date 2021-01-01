fab0059120801c3546ec5e36322ff2fbb1227d31
hananbn <76566773+hananbn@users.noreply.github.com>
2021-01-01 23:52:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function (window) {
     
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i in names) {
     
    
